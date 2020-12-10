347d7a5a54d7bd103c947f73b122a141763d47c0
Samuel Gaus <sam@gaus.co.uk>
2020-12-10 20:58:40 +0000

    Don't remove me from my clan

--- examples/clan.ts
+++ examples/clan.ts
@@ -2,5 +2,5 @@ import { clan } from "../src/index";
 
 clan.join("The Piglets of Fate");
 console.log(clan.getCurrentFax());
-clan.addPlayerToWhitelist("gausie");
-clan.removePlayerFromWhitelist("gausie");
+clan.addPlayerToWhitelist("onweb");
+clan.removePlayerFromWhitelist("onweb");

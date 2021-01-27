1c4bbe4ea6c9c0a3cef008b78ac3b5704e8605c0
Patrick Hulin <patrick.hulin@gmail.com>
2021-01-27 07:33:09 -0500

    Add SongBoom.

---
+++ src/resources/2018/SongBoom.ts
@@ -0,0 +1,61 @@
+import { cliExecute } from "kolmafia";
+import { have as haveItem } from "../../lib";
+import { get } from "../../property";
+import { $item } from "../../template-string";
+
+export const item = $item`SongBoom™ Boom Box`;
+export function have(): boolean {
+  return haveItem(item);
+}
+
+type SongBoomSong =
+  | "Eye of the Giger"
+  | "Food Vibrations"
+  | "Remainin' Alive"
+  | "These Fists Were Made for Punchin'"
+  | "Total Eclipse of Your Meat"
+  | null;
+
+export const songBoomSongs = new Set<string>([
+  "Eye of the Giger",
+  "Food Vibrations",
+  "Remainin' Alive",
+  "These Fists Were Made for Punchin'",
+  "Total Eclipse of Your Meat",
+]);
+
+/**
+ * Current song.
+ */
+export function song(): SongBoomSong {
+  const stored = get("boomBoxSong");
+  return songBoomSongs.has(stored) ? (stored as SongBoomSong) : null;
+}
+
+/**
+ * Song changes left today.
+ */
+export function songChangesLeft(): number {
+  return get("_boomBoxSongsLeft");
+}
+
+/**
+ * Change the song.
+ * @param newSong Song to change to.
+ */
+export function setSong(newSong: SongBoomSong): boolean {
+  if (song() !== newSong) {
+    if (songChangesLeft() === 0) throw new Error("Out of song changes!");
+    cliExecute(`boombox ${newSong}`);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * Progress to next song drop (e.g. gathered meat-clip).
+ */
+export function dropProgress(): number {
+  return get("_boomBoxFights");
+}

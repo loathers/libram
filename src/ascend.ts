cb43107974ea8529080c2ee7866252660d22fdfb
horrible little slime <69secret69email69@gmail.com>
2021-07-21 20:19:50 -0400

    add prepareAscension, which lets you set up your workshed, garden, eudora, and chateau as specified.

--- src/ascend.ts
+++ src/ascend.ts
@@ -1,5 +1,18 @@
-import { containsText, print, toInt, visitUrl } from "kolmafia";
-import { $item, $items, $stat } from ".";
+import {
+  containsText,
+  eudora,
+  eudoraItem,
+  getCampground,
+  getWorkshed,
+  print,
+  toInt,
+  use,
+  visitUrl,
+  xpath,
+} from "kolmafia";
+import { $item, $items, $stat, get, have } from ".";
+import { ChateauMantegna } from "./resources";
+import { educate } from "./resources/2016/SourceTerminal";
 
 export enum lifestyle {
   casual = 1,
@@ -148,7 +161,7 @@ export function ascend(
     Astral Ring, Astral Statuette,
     Astral Pistol, Astral Mask,
     Astral Pet Sweater, Astral Shirt,
-    Astral Blt`.includes(pet)
+    Astral Belt`.includes(pet)
   )
     throw `Invalid astral item ${pet}`;
 
@@ -166,4 +179,116 @@ export function ascend(
     }&asctype=${lifestyle}&nopetok=1&noskillsok=1&lamepathok=1&pwd`,
     true
   );
+}
+
+const worksheds = $items`Warbear LP-ROM Burner, Warbear Jackhammer Drill Press, Warbear Induction Oven, Warbear High-Efficiency Still, Warbear Chemistry Lab, Warbear Auto-Anvil, Spinning Wheel, Snow Machine, Little Geneticist DNA-Splicing Lab, Portable Mayo Clinic, Asdon Martin Keyfob, Diabolic Pizza Cube`;
+const gardens = $items`Packet of Pumpkin Seeds, Peppermint Pip Packet, Packet of Dragon's Teeth, Packet of Beer Seeds, Packet of Winter Seeds, Packet of Thanksgarden Seeds, Packet of Tall Grass Seeds, Packet of Mushroom Spores`;
+const eudorae = $items`My Own Pen-Pal Kit, GameInformPowerDailyPro Subscription Card, Xi Receiver Unit, New-You Club Membership Form`;
+
+const desks = $items`Fancy Stationary Set, Swiss Piggy Bank, Continental Juice Bar`;
+const ceilings = $items`Antler Chandelier, Ceiling Fan, Artificial Skylight`;
+const nightstands = $items`Foreign Language Tapes, Bottle of Potpourri, Electric Muscle Stimulator`;
+
+export function prepareAscension(
+  ascensionItems?: {
+    workshed?: Item;
+    garden?: Item;
+    eudora?: Item;
+  },
+  chateauItems?: {
+    desk?: Item;
+    ceiling?: Item;
+    nightstand?: Item;
+  },
+  throwOnFail = true
+): void {
+  if (ascensionItems) {
+    if (ascensionItems.workshed && getWorkshed() !== ascensionItems.workshed) {
+      if (!worksheds.includes(ascensionItems.workshed) && throwOnFail)
+        throw `Invalid workshed: ${ascensionItems.workshed}!`;
+      else if (!have(ascensionItems.workshed) && throwOnFail)
+        throw `I'm sorry buddy, but you don't seem to own a ${ascensionItems.workshed}. Which makes it REALLY hard for us to plop one into your workshed.`;
+      else if (get("_workshedItemUsed") && throwOnFail)
+        throw `Seems like you've already swapped your workshed, buddy.`;
+      else use(ascensionItems.workshed);
+      if (getWorkshed() !== ascensionItems.workshed && throwOnFail)
+        throw `We really thought we changed your workshed to a ${ascensionItems.workshed}, but Mafia is saying otherwise.`;
+    }
+
+    if (
+      ascensionItems.garden &&
+      !Object.getOwnPropertyNames(getCampground()).includes(
+        ascensionItems.garden.name
+      )
+    ) {
+      if (!gardens.includes(ascensionItems.garden) && throwOnFail)
+        throw `Invalid garden: ${ascensionItems.garden}!`;
+      else if (!have(ascensionItems.garden) && throwOnFail)
+        throw `I'm sorry buddy, but you don't seem to own a ${ascensionItems.garden}. Which makes it REALLY hard for us to plant one into your garden.`;
+      else use(ascensionItems.garden);
+      if (
+        !Object.getOwnPropertyNames(getCampground()).includes(
+          ascensionItems.garden.name
+        ) &&
+        throwOnFail
+      )
+        throw `We really thought we changed your garden to a ${ascensionItems.garden}, but Mafia is saying otherwise.`;
+    }
+
+    if (ascensionItems.eudora && eudoraItem() !== ascensionItems.eudora) {
+      if (!eudorae.includes(ascensionItems.eudora) && throwOnFail)
+        throw `Invalid eudora: ${ascensionItems.eudora}!`;
+      else if (
+        !xpath(
+          visitUrl("account.php?tab=correspondence"),
+          `/select[@name="whichpenpal"]/option/text()`
+        ).includes(ascensionItems.eudora.name) &&
+        throwOnFail
+      )
+        throw `I'm sorry buddy, but you don't seem to be subscribed to ${ascensionItems.eudora}. Which makes it REALLY hard to correspond with them.`;
+      else
+        visitUrl(
+          `account.php?actions[]=whichpenpal&whichpenpal=${
+            1 + eudorae.indexOf(ascensionItems.eudora)
+          }&action=Update`,
+          true
+        );
+      if (eudoraItem() !== ascensionItems.eudora && throwOnFail)
+        throw `We really thought we chaned your eudora to a ${ascensionItems.eudora}, but Mafia is saying otherwise.`;
+    }
+  }
+  if (chateauItems && ChateauMantegna.have()) {
+    if (
+      chateauItems.ceiling &&
+      ChateauMantegna.getCeiling() !== chateauItems.ceiling
+    ) {
+      if (!ceilings.includes(chateauItems.ceiling) && throwOnFail)
+        throw `Invalid chateau ceiling: ${chateauItems.ceiling}!`;
+      else if (
+        !ChateauMantegna.changeCeiling(chateauItems.ceiling) &&
+        throwOnFail
+      )
+        throw `We tried, but were unable to change your chateau ceiling to ${chateauItems.ceiling}. Probably.`;
+    }
+
+    if (chateauItems.desk && ChateauMantegna.getDesk() !== chateauItems.desk) {
+      if (!desks.includes(chateauItems.desk) && throwOnFail)
+        throw `Invalid chateau desk: ${chateauItems.desk}!`;
+      else if (!ChateauMantegna.changeDesk(chateauItems.desk) && throwOnFail)
+        throw `We tried, but were unable to change your chateau desk to ${chateauItems.desk}. Probably.`;
+    }
+
+    if (
+      chateauItems.nightstand &&
+      ChateauMantegna.getNightstand() !== chateauItems.nightstand
+    ) {
+      if (!nightstands.includes(chateauItems.nightstand) && throwOnFail)
+        throw `Invalid chateau nightstand: ${chateauItems.nightstand}!`;
+      else if (
+        !ChateauMantegna.changeNightstand(chateauItems.nightstand) &&
+        throwOnFail
+      )
+        throw `We tried, but were unable to change your chateau nightstand to ${chateauItems.nightstand}. Probably.`;
+    }
+  }
 }

8243adbdd982170990ef2abee4a2ae94902bc00e
horrible little slime <69secret69email69@gmail.com>
2021-07-26 23:59:32 -0400

    use native toLowerCase() method

--- tools/findOverlappingNames.ts
+++ tools/findOverlappingNames.ts
@@ -1,11 +1,23 @@
-import { toItem, toLowerCase, toSkill } from "kolmafia";
+import { toItem, toSkill } from "kolmafia";
 
 function overlappingSkills(): Skill[] {
-    const combatSkillNames = Skill.all().filter((skill) => skill.combat).map((skill) => toLowerCase(skill.name));
-    return combatSkillNames.filter((name1) => combatSkillNames.some((name2) => name2 !== name1 && name2.includes(name1))).map(toSkill)
+  const combatSkillNames = Skill.all()
+    .filter((skill) => skill.combat)
+    .map((skill) => skill.name.toLowerCase());
+  return combatSkillNames
+    .filter((name1) =>
+      combatSkillNames.some((name2) => name2 !== name1 && name2.includes(name1))
+    )
+    .map(toSkill);
 }
 
 function overlappingItems(): Item[] {
-    const combatItemNames = Item.all().filter((item) => item.combat).map((item) => toLowerCase(item.name));
-    return combatItemNames.filter((name1) => combatItemNames.some((name2) => name2 !== name1 && name2.includes(name1))).map(toItem)
+  const combatItemNames = Item.all()
+    .filter((item) => item.combat)
+    .map((item) => item.name.toLowerCase());
+  return combatItemNames
+    .filter((name1) =>
+      combatItemNames.some((name2) => name2 !== name1 && name2.includes(name1))
+    )
+    .map(toItem);
 }

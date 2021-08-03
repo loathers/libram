af953fd08d5d52c54943263cdc412e127d80b8d1
horrible little slime <69secret69email69@gmail.com>
2021-08-02 23:44:20 -0400

    create a Dungeon class

--- src/Basement.ts
+++ src/Basement.ts
@@ -9,12 +9,62 @@ import {
 } from "kolmafia";
 import { $items } from ".";
 
-type Dungeon = "Hobopolis" | "Dreadsylvania" | "The Slime Tube";
+class Dungeon {
+  name: string;
+  loot: Item[];
+  openAction: string;
+  closeAction: string;
+  openCost: number;
+  openImage: string;
+  closedImage: string;
 
-const Loot: Record<Dungeon, Item[]> = {
-  Hobopolis: $items`Ol' Scratch's ash can, Ol' Scratch's ol' britches, Ol' Scratch's stovepipe hat, Ol' Scratch's infernal pitchfork, Ol' Scratch's manacles, Ol' Scratch's stove door, Frosty's carrot, Frosty's nailbat, Frosty's old silk hat, Frosty's arm, Frosty's iceball, Frosty's snowball sack, Oscus's dumpster waders, Oscus's pelt, Wand of Oscus, Oscus's flypaper pants, Oscus's garbage can lid, Oscus's neverending soda, Zombo's grievous greaves, Zombo's shield, Zombo's skullcap, Zombo's empty eye, Zombo's shoulder blade, Zombo's skull ring, Chester's bag of candy, Chester's cutoffs, Chester's moustache, Chester's Aquarius medallion, Chester's muscle shirt, Chester's sunglasses, Hodgman's bow tie, Hodgman's porkpie hat, Hodgman's lobsterskin pants, Hodgman's almanac, Hodgman's lucky sock, Hodgman's metal detector, Hodgman's varcolac paw, Hodgman's harmonica, Hodgman's garbage sticker, Hodgman's cane, Hodgman's whackin' stick, Hodgman's disgusting technicolor overcoat, Hodgman's imaginary hamster`,
-  Dreadsylvania: $items`Great Wolf's headband, Great Wolf's right paw, Great Wolf's left paw, Great Wolf's lice, Great Wolf's rocket launcher, Great Wolf's beastly trousers, Drapes-You-Regally, Warms-Your-Tush, Covers-Your-Head, Protects-Your-Junk, Quiets-Your-Steps, Helps-You-Sleep, Mayor Ghost's khakis, Mayor Ghost's cloak, Mayor Ghost's toupee, Mayor Ghost's scissors, Mayor Ghost's sash, Mayor Ghost's gavel, zombie mariachi hat, zombie accordion, zombie mariachi pants, HOA regulation book, HOA zombie eyes, HOA citation pad, Unkillable Skeleton's skullcap, Unkillable Skeleton's shinguards, Unkillable Skeleton's breastplate, Unkillable Skeleton's shield, Unkillable Skeleton's sawsword, Unkillable Skeleton's restless leg, skull capacitor, Thunkula's drinking cap, Drunkula's silky pants, Drunkula's cape, Drunkula's ring of haze, Drunkula's wineglass, Drunkula's bell, bottle of Bloodweiser, bottle of Bloodweiser, bottle of Bloodweiser, bottle of Bloodweiser, electric Kool-Aid, electric Kool-Aid, electric Kool-Aid, electric Kool-Aid, ghost pepper, ghost pepper, ghost pepper, ghost pepper, Gets-You-Drunk, Gets-You-Drunk, Gets-You-Drunk, Gets-You-Drunk, wriggling severed nose, wriggling severed nose, wriggling severed nose, wriggling severed nose, Hunger™ Sauce, Hunger™ Sauce, Hunger™ Sauce, Hunger™ Sauce`,
-  "The Slime Tube": $items`slime-soaked brain, slime-soaked hypophysis, slime-soaked sweat gland, squirming Slime larva, caustic slime nodule, caustic slime nodule, hardened slime belt, hardened slime hat, hardened slime pants`,
+  constructor(
+    name: string,
+    loot: Item[],
+    openAction: string,
+    closeAction: string,
+    openCost: number,
+    openImage: string,
+    closedImage: string
+  ) {
+    this.name = name;
+    this.loot = loot;
+    this.openAction = openAction;
+    this.closeAction = closeAction;
+    this.openCost = openCost;
+    this.openImage = openImage;
+    this.closedImage = closedImage;
+  }
+}
+
+const Dungeons = {
+  Hobopolis: new Dungeon(
+    "Hobopolis",
+    $items`Ol' Scratch's ash can, Ol' Scratch's ol' britches, Ol' Scratch's stovepipe hat, Ol' Scratch's infernal pitchfork, Ol' Scratch's manacles, Ol' Scratch's stove door, Frosty's carrot, Frosty's nailbat, Frosty's old silk hat, Frosty's arm, Frosty's iceball, Frosty's snowball sack, Oscus's dumpster waders, Oscus's pelt, Wand of Oscus, Oscus's flypaper pants, Oscus's garbage can lid, Oscus's neverending soda, Zombo's grievous greaves, Zombo's shield, Zombo's skullcap, Zombo's empty eye, Zombo's shoulder blade, Zombo's skull ring, Chester's bag of candy, Chester's cutoffs, Chester's moustache, Chester's Aquarius medallion, Chester's muscle shirt, Chester's sunglasses, Hodgman's bow tie, Hodgman's porkpie hat, Hodgman's lobsterskin pants, Hodgman's almanac, Hodgman's lucky sock, Hodgman's metal detector, Hodgman's varcolac paw, Hodgman's harmonica, Hodgman's garbage sticker, Hodgman's cane, Hodgman's whackin' stick, Hodgman's disgusting technicolor overcoat, Hodgman's imaginary hamster`,
+    "cleansewer",
+    "floodsewer",
+    1000000,
+    "opengrate.gif",
+    "sewergrate.gif"
+  ),
+  Dreadsylvania: new Dungeon(
+    "Dreadsylvania",
+    $items`Great Wolf's headband, Great Wolf's right paw, Great Wolf's left paw, Great Wolf's lice, Great Wolf's rocket launcher, Great Wolf's beastly trousers, Drapes-You-Regally, Warms-Your-Tush, Covers-Your-Head, Protects-Your-Junk, Quiets-Your-Steps, Helps-You-Sleep, Mayor Ghost's khakis, Mayor Ghost's cloak, Mayor Ghost's toupee, Mayor Ghost's scissors, Mayor Ghost's sash, Mayor Ghost's gavel, zombie mariachi hat, zombie accordion, zombie mariachi pants, HOA regulation book, HOA zombie eyes, HOA citation pad, Unkillable Skeleton's skullcap, Unkillable Skeleton's shinguards, Unkillable Skeleton's breastplate, Unkillable Skeleton's shield, Unkillable Skeleton's sawsword, Unkillable Skeleton's restless leg, skull capacitor, Thunkula's drinking cap, Drunkula's silky pants, Drunkula's cape, Drunkula's ring of haze, Drunkula's wineglass, Drunkula's bell, bottle of Bloodweiser, bottle of Bloodweiser, bottle of Bloodweiser, bottle of Bloodweiser, electric Kool-Aid, electric Kool-Aid, electric Kool-Aid, electric Kool-Aid, ghost pepper, ghost pepper, ghost pepper, ghost pepper, Gets-You-Drunk, Gets-You-Drunk, Gets-You-Drunk, Gets-You-Drunk, wriggling severed nose, wriggling severed nose, wriggling severed nose, wriggling severed nose, Hunger™ Sauce, Hunger™ Sauce, Hunger™ Sauce, Hunger™ Sauce`,
+    "translatemap",
+    "foldmap",
+    1000000,
+    "dvmap.gif",
+    "foldmap.gif"
+  ),
+  SlimeTube: new Dungeon(
+    "The Slime Tube",
+    $items`slime-soaked brain, slime-soaked hypophysis, slime-soaked sweat gland, squirming Slime larva, caustic slime nodule, caustic slime nodule, hardened slime belt, hardened slime hat, hardened slime pants`,
+    "cleanspot",
+    "sealtube",
+    250000,
+    "slimehole.gif",
+    "greasespot.gif"
+  ),
 };
 
 type Player = {
@@ -36,14 +86,14 @@ export function distribute(
   const player = getPlayerFromIdOrName(idOrName);
   const lootList = Array.isArray(loot)
     ? loot
-    : typeof loot === "string"
-    ? Loot[loot]
+    : loot instanceof Dungeon
+    ? loot.loot
     : [loot];
   const badLoot = lootList.find(
     (individualLoot) =>
-      !Object.values(Loot).some((dungeonLoot) =>
-        dungeonLoot.includes(individualLoot)
-      )
+      !Object.values(Dungeons)
+        .map((dungeon) => dungeon.loot)
+        .some((dungeonLoot) => dungeonLoot.includes(individualLoot))
   );
   if (badLoot) {
     throw new Error(
@@ -75,45 +125,29 @@ export function distribute(
 }
 
 export function closeDungeon(dungeon: Dungeon): boolean {
-  const action =
-    dungeon === "Hobopolis"
-      ? "floodsewer"
-      : dungeon === "The Slime Tube"
-      ? "sealtube"
-      : "foldmap";
-  visitUrl(`clan_basement.php?action=${action}&confirm=true`);
+  visitUrl(`clan_basement.php?action=${dungeon.closeAction}&confirm=true`);
   const pageText = visitUrl("clan_basement.php");
-  const imageToCheck =
-    dungeon === "Hobopolis"
-      ? "opengrate.gif"
-      : dungeon === "The Slime Tube"
-      ? "slimehole.gif"
-      : "dvmap.gif";
-  return !pageText.includes(imageToCheck);
+  return pageText.includes(dungeon.closedImage);
 }
 
 export function openDungeon(
   dungeon: Dungeon,
   paymentPolicy: "None" | "All" | "Difference" = "Difference"
-) {
-  const imageToCheck =
-    dungeon === "Hobopolis"
-      ? "opengrate.gif"
-      : dungeon === "The Slime Tube"
-      ? "slimehole.gif"
-      : "dvmap.gif";
+): boolean {
   const pageText = visitUrl("clan_basement.php");
-  if (pageText.includes(imageToCheck)) return true;
+  if (pageText.includes(dungeon.openImage)) return true;
 
-  const cost = dungeon === "The Slime Tube" ? 250000 : 1000000;
   if (paymentPolicy === "All") {
-    visitUrl(`clan_stash.php?action=contribute&howmuch=${cost}`, true);
+    visitUrl(
+      `clan_stash.php?action=contribute&howmuch=${dungeon.openCost}`,
+      true
+    );
   } else {
     const stashText = visitUrl("clan_stash.php").match(
       /Meat to the coffer \(You have (\S+) Meat.)/g
     );
     const stashMeat = stashText ? parseInt(stashText[0].replace(",", "")) : 0;
-    const payDifference = cost - stashMeat;
+    const payDifference = dungeon.openCost - stashMeat;
     if (payDifference > 0) {
       if (paymentPolicy === "None") return false;
       visitUrl(
@@ -122,5 +156,6 @@ export function openDungeon(
       );
     }
   }
-  const action = "uh oh";
+  visitUrl(`clan_basement.php?action=${dungeon.openAction}`, true);
+  return visitUrl("clan_basement.php").includes(dungeon.openImage);
 }

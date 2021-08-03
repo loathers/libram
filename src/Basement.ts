a435a7b4cb2c4f8d03a167c99d00d69dc898713a
horrible little slime <69secret69email69@gmail.com>
2021-08-02 23:49:37 -0400

    export Dungeons

--- src/Basement.ts
+++ src/Basement.ts
@@ -37,7 +37,7 @@ class Dungeon {
   }
 }
 
-const Dungeons = {
+export const Dungeons = {
   Hobopolis: new Dungeon(
     "Hobopolis",
     $items`Ol' Scratch's ash can, Ol' Scratch's ol' britches, Ol' Scratch's stovepipe hat, Ol' Scratch's infernal pitchfork, Ol' Scratch's manacles, Ol' Scratch's stove door, Frosty's carrot, Frosty's nailbat, Frosty's old silk hat, Frosty's arm, Frosty's iceball, Frosty's snowball sack, Oscus's dumpster waders, Oscus's pelt, Wand of Oscus, Oscus's flypaper pants, Oscus's garbage can lid, Oscus's neverending soda, Zombo's grievous greaves, Zombo's shield, Zombo's skullcap, Zombo's empty eye, Zombo's shoulder blade, Zombo's skull ring, Chester's bag of candy, Chester's cutoffs, Chester's moustache, Chester's Aquarius medallion, Chester's muscle shirt, Chester's sunglasses, Hodgman's bow tie, Hodgman's porkpie hat, Hodgman's lobsterskin pants, Hodgman's almanac, Hodgman's lucky sock, Hodgman's metal detector, Hodgman's varcolac paw, Hodgman's harmonica, Hodgman's garbage sticker, Hodgman's cane, Hodgman's whackin' stick, Hodgman's disgusting technicolor overcoat, Hodgman's imaginary hamster`,
@@ -65,7 +65,7 @@ const Dungeons = {
     "slimehole.gif",
     "greasespot.gif"
   ),
-};
+} as const;
 
 type Player = {
   name: string;

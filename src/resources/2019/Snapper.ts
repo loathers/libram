dcb637a753d10adfa1c634ec379bbe42a99e4604
Patrick Hulin <patrick.hulin@gmail.com>
2021-10-01 23:33:47 -0400

    add items

--- src/resources/2019/Snapper.ts
+++ src/resources/2019/Snapper.ts
@@ -9,6 +9,35 @@ import { get } from "../../property";
 
 const familiar = Familiar.get("Red-Nosed Snapper");
 
+export const phylumItem = new Map<Phylum, Item>([
+  [Phylum.get("Beast"), Item.get("patch of extra-warm fur")],
+  [Phylum.get("Bug"), Item.get("a bug's lymph")],
+  [Phylum.get("Constellation"), Item.get("micronova")],
+  [Phylum.get("Construct"), Item.get("industrial lubricant")],
+  [Phylum.get("Demon"), Item.get("infernal snowball")],
+  [Phylum.get("Dude"), Item.get("human musk")],
+  [Phylum.get("Elemental"), Item.get("livid energy")],
+  [Phylum.get("Elf"), Item.get("peppermint syrup")],
+  [Phylum.get("Fish"), Item.get("fish sauce")],
+  [Phylum.get("Goblin"), Item.get("guffin")],
+  [Phylum.get("Hippie"), Item.get("organic potpourri")],
+  [Phylum.get("Hobo"), Item.get("beggin' cologne")],
+  [Phylum.get("Horror"), Item.get("powdered madness")],
+  [Phylum.get("Humanoid"), Item.get("vial of humanoid growth hormone")],
+  [Phylum.get("Mer-kin"), Item.get("Mer-kin eyedrops")],
+  [Phylum.get("Orc"), Item.get("boot flask")],
+  [Phylum.get("Penguin"), Item.get("envelope full of Meat")],
+  [Phylum.get("Pirate"), Item.get("Shantix™")],
+  [Phylum.get("Plant"), Item.get("goodberry")],
+  [Phylum.get("Slime"), Item.get("extra-strength goo")],
+  [Phylum.get("Undead"), Item.get("unfinished pleasure")],
+  [Phylum.get("Weird"), Item.get("non-Euclidean angle]")],
+]);
+
+export const itemPhylum = new Map(
+  [...phylumItem].map(([phylum, item]) => [item, phylum])
+);
+
 /**
  * Return whether you have a Red-Nosed Snapper.
  * @returns True if you have a Red-Nosed Snapper, false otherwise.

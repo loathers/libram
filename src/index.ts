58bd0367f28cfb927e7800677c5d31d66f76f44f
horrible little slime <69secret69email69@gmail.com>
2021-11-09 13:08:52 -0500

    export modifiers

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ export {
   withChoice,
 } from "./property";
 export { get as getModifier } from "./modifier";
+export { Modifiers } from "./modifier";
 export * from "./freerun";
 export * as Dreadsylvania from "./dungeons/Dreadsylvania";
 export * as Hobopolis from "./dungeons/Hobopolis";

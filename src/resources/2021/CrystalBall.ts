b064fce09a094cec70efa810370a9c9d4e0a3997
horrible little slime <69secret69email69@gmail.com>
2021-12-14 22:45:34 -0500

    basic implementation

---
+++ src/resources/2021/CrystalBall.ts
@@ -0,0 +1,24 @@
+import { myTurncount, toLocation, toMonster } from "kolmafia";
+import { get } from "../../property";
+
+const parsedProp = () =>
+  get("crystalBallPredictions")
+    .split("|")
+    .map((element) => element.split(":") as [string, string, string])
+    .map(
+      ([turncount, location, monster]) =>
+        [parseInt(turncount), toLocation(location), toMonster(monster)] as [
+          number,
+          Location,
+          Monster
+        ]
+    );
+
+export function currentPredictions(): Map<Location, Monster> {
+  const predictions = parsedProp();
+  return new Map(
+    predictions
+      .filter(([turncount]) => turncount === myTurncount())
+      .map(([, location, monster]) => [location, monster])
+  );
+}

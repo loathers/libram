dc62dfe92c9cd7087712e7875b2773bd10453f7d
horrible little slime <69secret69email69@gmail.com>
2021-12-29 23:57:07 -0500

    comments & jsdocs

--- src/lib.ts
+++ src/lib.ts
@@ -658,9 +658,17 @@ export function getTodaysHolidayWanderers(): Monster[] {
     .flat();
 }
 
+/**
+ * Returns null for counters that do not exist, and otherwise returns the duration of the counter
+ * @param counter The name of the counter in question
+ * @returns null if the counter does not exist; otherwise returns the duration of the counter
+ */
 export function getCounterValue(counter: string): number | null {
   const value = getCounter(counter);
+  //getCounter returns -1 for counters that don't exist, but it also returns -1 for counters whose value is -1
   if (value === -1) {
+    //if we have a counter with value -1, we check to see if that counter exists via getCounters()
+    //We return null if it doesn't exist
     return getCounters(counter, -1, -1).trim() === "" ? null : -1;
   }
   return value;

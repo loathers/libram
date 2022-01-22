b948443fb9ec9ab1b93927d98e5ca9ec94ac542b
horrible little slime <69secret69email69@gmail.com>
2022-01-21 22:07:00 -0500

    rename folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./ascend";
 export * from "./Clan";
-export * from "./challengepaths";
+export * from "./challengePaths";
 export * from "./combat";
 export * as Counter from "./counter";
 export * from "./diet";

06036d4a1a6e32ac6f22c9de48645d3311e68fdd
horrible little slime <69secret69email69@gmail.com>
2022-02-01 00:18:31 -0500

    fix inequality for fractional part of potion mood elements

--- src/mood.ts
+++ src/mood.ts
@@ -197,7 +197,7 @@ class PotionMoodElement extends MoodElement {
     if (mallPrice(this.potion) > this.maxPricePerTurn * turnsPerUse) {
       return false;
     }
-    //integer part
+    // integer part
     if (effectTurns < ensureTurns) {
       const uses = Math.floor((ensureTurns - effectTurns) / turnsPerUse);
       const quantityToBuy = clamp(uses - availableAmount(this.potion), 0, 100);
@@ -210,11 +210,11 @@ class PotionMoodElement extends MoodElement {
       use(quantityToUse, this.potion);
     }
 
-    //fractional part
+    // fractional part
     const remainingDifference = ensureTurns - haveEffect(effect);
     if (remainingDifference > 0) {
       const price = this.maxPricePerTurn * remainingDifference;
-      if (price <= mallPrice(this.potion)) {
+      if (price >= mallPrice(this.potion)) {
         if (
           availableAmount(this.potion) ||
           buy(1, this.potion, Math.floor(price))

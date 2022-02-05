5c1873d2b8c7f7f918b8d4dedecf386ea2dc6c9f
horrible little slime <69secret69email69@gmail.com>
2022-02-05 13:52:58 -0500

    docstrings

--- src/resources/2022/CombatLoversLocket.ts
+++ src/resources/2022/CombatLoversLocket.ts
@@ -2,6 +2,7 @@ import {
   cliExecute,
   getLocketMonsters,
   Monster,
+  runCombat,
   setAutoAttack,
   toMonster,
 } from "kolmafia";
@@ -12,32 +13,54 @@ import { $item } from "../../template-string";
 import { clamp } from "../../utils";
 
 // eslint-disable-next-line libram/verify-constants
-const locket = $item`Combat Lover's Locket`;
+export const locket = $item`Combat Lover's Locket`;
 
 export function have(): boolean {
   return haveItem(locket);
 }
 
+/**
+ * Filters the set of all unlocked locket monsters to only the ones available to be locketed right now.
+ * @returns An array consisting of all Monsters you can fight with your locket right now.
+ */
 export function availableLocketMonsters(): Monster[] {
+  if (reminiscesLeft() === 0) return [];
   return Object.entries(getLocketMonsters())
     .filter(([, unused]) => unused)
     .map(([name]) => toMonster(name));
 }
 
+/**
+ * Parses getLocketMonsters and returns the collection of all Monsters as an Array.
+ * @returns An array consisting of all Monsters you can hypothetically fight, regardless of whether they've been fought today.
+ */
 export function unlockedLocketMonsters(): Monster[] {
   return Object.entries(getLocketMonsters()).map(([name]) => toMonster(name));
 }
 
+/**
+ * Determines how many reminisces remain by parsing the _locketMonstersFought property.
+ * @returns The number of reminisces a player has available; 0 if they lack the Locket.
+ */
 export function reminiscesLeft(): number {
-  return clamp(3 - get("_locketMonstersFought").split(",").length, 0, 3);
+  return have()
+    ? clamp(3 - get("_locketMonstersFought").split(",").length, 0, 3)
+    : 0;
 }
 
 export function monstersReminisced(): Monster[] {
   return get("_locketMonstersFought")
     .split(",")
-    .map((name) => toMonster(name));
+    .map((id) => toMonster(parseInt(id)));
 }
 
+/**
+ * Fight a Monster using the Combat Lover's Locket
+ * @param monster The Monster to fight
+ * @param macro A Macro or Macro-extending-class-instance used to dictate our actions during this combat
+ * @param useAuto Whether we should use an autoattack to resolve this combat
+ * @returns false if we are unable to reminisce about this monster. Else, returns whether, at the end of all things, we have reminisced about this monster.
+ */
 export function reminisce<T extends Macro>(
   monster: Monster,
   macro: T,
@@ -57,6 +80,7 @@ export function reminisce<T extends Macro>(
 
   try {
     cliExecute(`reminisce ${monster}`);
+    runCombat();
   } finally {
     Macro.clearSaved();
     if (useAuto) setAutoAttack(0);

3921d62a62fd54c9643dff49f06ff606c736151f
horrible little slime <69secret69email69@gmail.com>
2022-02-16 22:34:25 -0500

    change return types on CommunityService.run()

--- src/challengePaths/2015/CommunityService.ts
+++ src/challengePaths/2015/CommunityService.ts
@@ -132,12 +132,16 @@ export default class CommunityService {
    * @param prepare A function that does all necessary preparations for this CS test, including choosing your outfit.
    * @param beCertain Whether we should check council.php instead of mafia to determine whether the test is complete.
    * @param maxTurns We will run the test iff the predicted turns is less than or equal to this parameter.
-   * @returns The output of the prepare function given, or null if the test is already complete.
+   * @returns "completed", "failed", or "already completed".
    */
-  run(prepare: () => void, beCertain = false, maxTurns = Infinity): boolean {
+  run(
+    prepare: () => void,
+    beCertain = false,
+    maxTurns = Infinity
+  ): "completed" | "failed" | "already completed" {
     const finishedFunction = () =>
       beCertain ? this.verifyIsDone() : this.isDone();
-    if (finishedFunction()) return false;
+    if (finishedFunction()) return "already completed";
 
     const startTime = Date.now();
     const startTurns = myTurncount();
@@ -145,7 +149,7 @@ export default class CommunityService {
     try {
       prepare();
     } catch {
-      return false;
+      return "failed";
     }
 
     const prediction = this.predictor();
@@ -160,9 +164,9 @@ export default class CommunityService {
         turnCost: myTurncount() - startTurns,
         seconds: (Date.now() - startTime) / 1000,
       };
-      return true;
+      return "completed";
     }
-    return false;
+    return "failed";
   }
 
   /**

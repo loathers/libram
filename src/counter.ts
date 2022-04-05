7c6e309f108efc87c8d594bb3149e214eddd63f4
horrible little slime <69secret69email69@gmail.com>
2022-04-05 18:48:46 -0400

    return Infinity instead of null for counters

--- src/counter.ts
+++ src/counter.ts
@@ -5,13 +5,13 @@ import { cliExecute, getCounter, getCounters } from "kolmafia";
  * @param counter The name of the counter in question
  * @returns null if the counter does not exist; otherwise returns the duration of the counter
  */
-export function get(counter: string): number | null {
+export function get(counter: string): number {
   const value = getCounter(counter);
   //getCounter returns -1 for counters that don't exist, but it also returns -1 for counters whose value is -1
   if (value === -1) {
     //if we have a counter with value -1, we check to see if that counter exists via getCounters()
     //We return null if it doesn't exist
-    return getCounters(counter, -1, -1).trim() === "" ? null : -1;
+    return getCounters(counter, -1, -1).trim() === "" ? Infinity : -1;
   }
   return value;
 }

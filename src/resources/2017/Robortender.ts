1eed8560340222297502bf62c344c540d0aadac6
horrible little slime <69secret69email69@gmail.com>
2022-05-18 21:05:52 -0400

    initial implementation

---
+++ src/resources/2017/Robortender.ts
@@ -0,0 +1,56 @@
+import { haveFamiliar, Item, Monster, Phylum } from "kolmafia";
+import { get } from "../../property";
+import { $familiar, $item, $phylum } from "../../template-string";
+
+export const familiar = $familiar`Robortender`;
+
+export function have(): boolean {
+  return haveFamiliar(familiar);
+}
+
+const phylumDrops = new Map<Phylum, Item>([
+  [$phylum`Bug`, $item`pickled grasshopper`],
+  [$phylum`Constellation`, $item`bottle of anís`],
+  [$phylum`Demon`, $item`bottle of novelty hot sauce`],
+  [$phylum`Elemental`, $item`elemental sugarcube`],
+  [$phylum`Elf`, $item`peppermint sprig`],
+  [$phylum`Fish`, $item`bottle of clam juice`],
+  [$phylum`Goblin`, $item`cocktail mushroom`],
+  [$phylum`Hippy`, $item`shot of granola liqueur`],
+  [$phylum`Hobo`, $item`can of cherry-flavored sterno`],
+  [$phylum`Horror`, $item`lump of black ichor`],
+  [$phylum`Humanoid`, $item`bottle of gregnadigne`],
+  [$phylum`Mer-kin`, $item`bottle of Crème de Fugu`],
+  [$phylum`Orc`, $item`baby oil shooter`],
+  [$phylum`Penguin`, $item`fish head`],
+  [$phylum`Pirate`, $item`limepatch`],
+  [$phylum`Plant`, $item`pile of dirt`],
+  [$phylum`Slime`, $item`slime shooter`],
+  [$phylum`Weird`, $item`imaginary lemon`],
+]);
+
+export function dropFrom(target: Monster | Phylum): Item {
+  const phylum = target instanceof Monster ? target.phylum : target;
+  return phylumDrops.get(phylum) ?? $item`none`;
+}
+
+export function dropChance(): number {
+  switch (get("_roboDrops")) {
+    case 0:
+      return 1;
+    case 1:
+    case 2:
+    case 3:
+      return 0.5;
+    case 4:
+    case 5:
+    case 6:
+      return 0.4;
+    case 7:
+    case 8:
+    case 9:
+      return 0.3;
+    default:
+      return 0.2;
+  }
+}

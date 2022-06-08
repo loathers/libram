0057c61c76b85e81a71c835693e9ca321d81fbfe
horrible little slime <69secret69email69@gmail.com>
2022-06-08 12:32:03 -0400

    document

--- src/resources/2022/JuneCleaver.ts
+++ src/resources/2022/JuneCleaver.ts
@@ -7,14 +7,23 @@ export function have(): boolean {
   return availableAmount(cleaver) > 0;
 }
 
+/**
+ * @returns The number of cleaver-combats it currently takes to get a choice--this is agnostic of your current fights.
+ */
 export function getInterval(): number {
   return [1, 6, 10, 12, 15, 20][get("_juneCleaverEncounters")] ?? 30;
 }
 
+/**
+ * @returns The number of fights until your next cleaver choice.
+ */
 export function fightsToNextChoice(): number {
   return getInterval() - get("_juneCleaverCharge");
 }
 
+/**
+ * @returns The bonus damage your cleaver currently gives for a given element.
+ */
 export function damage(
   element: "Hot" | "Stench" | "Sleaze" | "Spooky" | "Cold"
 ): number {

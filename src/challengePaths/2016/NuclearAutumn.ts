98368554f5c780fdc80f4ea8d34cae9727c1740d
Tokoeka <nzgamer666@gmail.com>
2022-09-19 23:48:02 +0100

    Add some docstrings

--- src/challengePaths/2016/NuclearAutumn.ts
+++ src/challengePaths/2016/NuclearAutumn.ts
@@ -1,13 +1,22 @@
 import { visitUrl } from "kolmafia";
 
+/**
+ * Visits the Cooling Tank on level 8 of the Fallout shelter to gain 300 rads
+ */
 export function coolingTank(): void {
   visitUrl("place.php?whichplace=falloutshelter&action=vault8");
 }
 
+/**
+ * Visits the Spa Simulation Chamber on level 4 of the Fallout shelter for 100 turns of "100% all stats"
+ */
 export function spa(): void {
   visitUrl("place.php?whichplace=falloutshelter&action=vault3");
 }
 
+/**
+ * Visits the Chronodynamics Laboratory on level 5 of the Fallout shelter to permanently increase radiation level by 3
+ */
 export function chronoLab(): void {
   visitUrl("place.php?whichplace=falloutshelter&action=vault5");
 }

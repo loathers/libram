84a5b963e0a02c566a13c38e744d099d324f1399
horrible little slime <69secret69email69@gmail.com>
2022-10-07 21:36:39 -0400

    Docstrings

--- src/resources/2022/AutumnAton.ts
+++ src/resources/2022/AutumnAton.ts
@@ -14,10 +14,16 @@ import { get } from "../../property";
 
 export const item = Item.get("Autumn-Aton");
 
+/**
+ * Is the autumn-aton currently in your inventory, available to deploy?
+ */
 export function available(): boolean {
   return availableAmount(item) > 0;
 }
 
+/**
+ * Do you own the autumn-aton?
+ */
 export function have(): boolean {
   return get("hasAutumnaton") || available();
 }
@@ -31,10 +37,19 @@ function checkLocations(html: string): Location[] {
 
 const use = () => visitUrl("inv_use.php?pwd&whichitem=10954");
 
+/**
+ * @returns The current location the autumn-aton is questing in; null if it is not on a quest.
+ */
 export function currentlyIn(): Location | null {
   return get("autumnatonQuestLocation");
 }
 
+/**
+ * Deploy the autumn-aton to a location of your choosing.
+ * @param target A location to send the autumn-aton to, or a prioritized list of locations to send it to, or a function to pick which location to send it to.
+ * @param upgrade Should we apply any upgrades we see available?
+ * @returns Where we ended up sending the autumn-aton; null if we didn't send it off.
+ */
 export function sendTo(
   target: Location | Location[] | ((locations: Location[]) => Location),
   upgrade = true
@@ -60,6 +75,10 @@ export function sendTo(
   return currentlyIn();
 }
 
+/**
+ * Install any available upgrades for the autumn-aton.
+ * @returns Whether there were any upgrades to install.
+ */
 export function upgrade(): boolean {
   use();
   const canUpgrade = availableChoiceOptions()[1] !== undefined;
@@ -68,6 +87,9 @@ export function upgrade(): boolean {
   return canUpgrade;
 }
 
+/**
+ * @returns A list of all locations you can send your autumn-aton to right now. Empty if you are unable to send it anywhere.
+ */
 export function availableLocations(): Location[] {
   if (!available()) return [];
   const pageHtml = use();
@@ -75,6 +97,9 @@ export function availableLocations(): Location[] {
   return checkLocations(pageHtml);
 }
 
+/**
+ * The mafia names for the autumn-aton upgrades
+ */
 export const possibleUpgrades = [
   "leftarm1",
   "leftleg1",
@@ -88,14 +113,23 @@ export const possibleUpgrades = [
 ] as const;
 export type Upgrade = typeof possibleUpgrades[number];
 
+/**
+ * @returns An array containing the upgrades that you currently have on your autumn-aton.
+ */
 export function currentUpgrades(): Upgrade[] {
   return get("autumnatonUpgrades").split(",") as Upgrade[];
 }
 
+/**
+ * @returns The number of turns remaining in your current autumn-aton quest. This number may be negative for any number of reasons.
+ */
 export function turnsLeft(): number {
   return get("autumnatonQuestTurn") - totalTurnsPlayed();
 }
 
+/**
+ * @returns The number of turns we expect your next autumn-aton quest to take.
+ */
 export function turnsForQuest(): number {
   return (
     11 *
@@ -107,6 +141,9 @@ export function turnsForQuest(): number {
   );
 }
 
+/**
+ * @returns The current visual acuity level of your autumn-aton as determined by the current upgrade-state.
+ */
 export function visualAcuity(): 1 | 2 | 3 {
   const visualUpgrades = ["periscope", "radardish"];
   return (1 +
@@ -116,6 +153,9 @@ export function visualAcuity(): 1 | 2 | 3 {
     | 3;
 }
 
+/**
+ * @returns The number of items from a zone we expect the autumn-aton to steal based on the current upgrade-state. It may not succeed in stealing every item it can.
+ */
 export function zoneItems(): 3 | 4 | 5 {
   return (3 + currentUpgrades().filter((u) => u.includes("arm")).length) as
     | 3
@@ -123,6 +163,9 @@ export function zoneItems(): 3 | 4 | 5 {
     | 5;
 }
 
+/**
+ * @returns The number of seasonal items we expect the autumn-aton to return with given its current upgrade-state.
+ */
 export function seasonalItems(): 1 | 2 {
   return currentUpgrades().includes("cowcatcher") ? 2 : 1;
 }

35affe52be12739c5d14ea268887da0ab80587f7
horrible little slime <69secret69email69@gmail.com>
2023-04-12 17:18:57 -0400

    stop using directlyUse for barrelshrine

--- src/resources/2015/BarrelShrine.ts
+++ src/resources/2015/BarrelShrine.ts
@@ -1,7 +1,6 @@
-import { availableAmount, runChoice } from "kolmafia";
-import { directlyUse } from "../../lib";
+import { availableAmount, runChoice, visitUrl } from "kolmafia";
 import { get } from "../../property";
-import { $item, $items } from "../../template-string";
+import { $items } from "../../template-string";
 import { sum } from "../../utils";
 
 const BARRELS = $items`little firkin, normal barrel, big tun, weathered barrel, dusty barrel, disintegrating barrel, moist barrel, rotting barrel, mouldering barrel, barnacled barrel`;
@@ -22,7 +21,7 @@ export function smashParty() {
   const total = sum(BARRELS, availableAmount);
   if (total <= 0) return;
 
-  directlyUse($item`little firkin`);
+  visitUrl("inv_use.php?pwd&whichitem=8568&choice=1");
 
   for (let i = 0; i < total / 100; i++) {
     runChoice(2);

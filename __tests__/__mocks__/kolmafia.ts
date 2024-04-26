34fa0836f57fbf01d09a06b4be1125f8871f1133
Bernhard Frauendienst <git@nospam.obeliks.de>
2024-04-26 23:03:16 +0200

    test: fix and extend kolmafia mock
    Automatically mock every function from kolmafia and provide dummy
    implementation for some (not a conclusive list)

--- __tests__/__mocks__/kolmafia.ts
+++ __tests__/__mocks__/kolmafia.ts
@@ -1,15 +1,20 @@
+import { jest } from "@jest/globals";
+
+import type RuntimeLibrary from "kolmafia";
+import Mocked = jest.Mocked;
+
+type Kolmafia = typeof RuntimeLibrary;
+
+type N = number | string | (number | string)[];
+
 function mockOneOrMany<
   Ctor extends new (name: string) => T,
   T extends MafiaClass
->(
-  ctor: Ctor,
-  n: number | string | (number | string)[],
-  knownInstances: T[]
-): T | T[] {
+>(ctor: Ctor, n: N, knownInstances: T[]): T | T[] {
   function mockOne(name: number | string): T {
     // to make mocking easier, we'll just tread ids as names
     if (typeof name === "number") {
-      name = name.toString();
+      name = `[${name.toString()}]`;
     }
     const existingInstance = knownInstances.find((i) => i.name === name);
     if (existingInstance) {
@@ -30,53 +35,79 @@ abstract class MafiaClass {
   constructor(readonly name: string) {}
 }
 
-export class Bounty extends MafiaClass {
+class Bounty extends MafiaClass {
   private static knownInstances: Bounty[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Bounty, n, Bounty.knownInstances));
+
+  static get = jest.fn((n: N) =>
+    mockOneOrMany(Bounty, n, Bounty.knownInstances)
+  );
+
   static all = jest.fn(() => Bounty.knownInstances);
+
   static none = {};
 }
-export class Class extends MafiaClass {
+class Class extends MafiaClass {
   private static knownInstances: Class[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Class, n, Class.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Class, n, Class.knownInstances));
+
   static all = jest.fn(() => Class.knownInstances);
+
   static none = {};
 }
-export class Coinmaster extends MafiaClass {
+class Coinmaster extends MafiaClass {
   private static knownInstances: Coinmaster[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Coinmaster, n, Coinmaster.knownInstances)
   );
+
   static all = jest.fn(() => Coinmaster.knownInstances);
+
   static none = {};
 }
-export class Effect extends MafiaClass {
+class Effect extends MafiaClass {
   private static knownInstances: Effect[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Effect, n, Effect.knownInstances));
+
+  static get = jest.fn((n: N) =>
+    mockOneOrMany(Effect, n, Effect.knownInstances)
+  );
+
   static all = jest.fn(() => Effect.knownInstances);
+
   static none = {};
 }
-export class Element extends MafiaClass {
+class Element extends MafiaClass {
   private static knownInstances: Element[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Element, n, Element.knownInstances)
   );
+
   static all = jest.fn(() => Element.knownInstances);
+
   static none = {};
 }
-export class Familiar extends MafiaClass {
+class Familiar extends MafiaClass {
   private static knownInstances: Familiar[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Familiar, n, Familiar.knownInstances)
   );
+
   static all = jest.fn(() => Familiar.knownInstances);
+
   static none = {};
 }
-export class Item extends MafiaClass {
+class Item extends MafiaClass {
   private static knownInstances: Item[] = [];
+
   private static mockId = 11;
-  static get = jest.fn((n) => mockOneOrMany(Item, n, Item.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Item, n, Item.knownInstances));
+
   static all = jest.fn(() => Item.knownInstances);
+
   static none = new Item("", -1, "");
 
   constructor(
@@ -88,89 +119,184 @@ export class Item extends MafiaClass {
   }
 }
 
-export class Location extends MafiaClass {
+class Location extends MafiaClass {
   private static knownInstances: Location[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Location, n, Location.knownInstances)
   );
+
   static all = jest.fn(() => Location.knownInstances);
+
   static none = {};
 }
-export class Modifier extends MafiaClass {
+class Modifier extends MafiaClass {
   private static knownInstances: Modifier[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Modifier, n, Modifier.knownInstances)
   );
+
   static all = jest.fn(() => Modifier.knownInstances);
+
   static none = {};
 }
-export class Monster extends MafiaClass {
+class Monster extends MafiaClass {
   private static knownInstances: Monster[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Monster, n, Monster.knownInstances)
   );
+
   static all = jest.fn(() => Monster.knownInstances);
+
   static none = {};
 }
-export class Path extends MafiaClass {
+class Path extends MafiaClass {
   private static knownInstances: Path[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Path, n, Path.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Path, n, Path.knownInstances));
+
   static all = jest.fn(() => Path.knownInstances);
+
   static none = {};
 }
-export class Phylum extends MafiaClass {
+class Phylum extends MafiaClass {
   private static knownInstances: Phylum[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Phylum, n, Phylum.knownInstances));
+
+  static get = jest.fn((n: N) =>
+    mockOneOrMany(Phylum, n, Phylum.knownInstances)
+  );
+
   static all = jest.fn(() => Phylum.knownInstances);
+
   static none = {};
 }
-export class Servant extends MafiaClass {
+class Servant extends MafiaClass {
   private static knownInstances: Servant[] = [];
-  static get = jest.fn((n) =>
+
+  static get = jest.fn((n: N) =>
     mockOneOrMany(Servant, n, Servant.knownInstances)
   );
+
   static all = jest.fn(() => Servant.knownInstances);
+
   static none = {};
 }
-export class Skill extends MafiaClass {
+class Skill extends MafiaClass {
   private static knownInstances: Skill[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Skill, n, Skill.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Skill, n, Skill.knownInstances));
+
   static all = jest.fn(() => Skill.knownInstances);
+
   static none = {};
 }
-export class Slot extends MafiaClass {
+class Slot extends MafiaClass {
   private static knownInstances: Slot[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Slot, n, Slot.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Slot, n, Slot.knownInstances));
+
   static all = jest.fn(() => Slot.knownInstances);
+
   static none = {};
 }
-export class Stat extends MafiaClass {
+class Stat extends MafiaClass {
   private static knownInstances: Stat[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Stat, n, Stat.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Stat, n, Stat.knownInstances));
+
   static all = jest.fn(() => Stat.knownInstances);
+
   static none = {};
 }
-export class Thrall extends MafiaClass {
+class Thrall extends MafiaClass {
   private static knownInstances: Thrall[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Thrall, n, Thrall.knownInstances));
+
+  static get = jest.fn((n: N) =>
+    mockOneOrMany(Thrall, n, Thrall.knownInstances)
+  );
+
   static all = jest.fn(() => Thrall.knownInstances);
+
   static none = {};
 }
-export class Vykea extends MafiaClass {
+class Vykea extends MafiaClass {
   private static knownInstances: Vykea[] = [];
-  static get = jest.fn((n) => mockOneOrMany(Vykea, n, Vykea.knownInstances));
+
+  static get = jest.fn((n: N) => mockOneOrMany(Vykea, n, Vykea.knownInstances));
+
   static all = jest.fn(() => Vykea.knownInstances);
+
   static none = {};
 }
 
-export const getPlayerName = jest.fn();
-export const getPlayerId = jest.fn();
+const kolmafiaClasses = {
+  Bounty,
+  Class,
+  Coinmaster,
+  Effect,
+  Element,
+  Familiar,
+  Item,
+  Location,
+  Modifier,
+  Monster,
+  Path,
+  Phylum,
+  Servant,
+  Skill,
+  Slot,
+  Stat,
+  Thrall,
+  Vykea,
+};
+const kolmafiaMocks: Record<string, unknown> = {
+  ...kolmafiaClasses,
+};
 
-export const getProperty = jest.fn();
+const actualKolmafia = jest.requireActual("kolmafia") as Kolmafia;
 
-export const isGiftable = jest.fn();
+// we have to mock the functions ourselves, because with automock all functions
+// share the same `warn` mock
+for (const [key, value] of Object.entries(actualKolmafia)) {
+  if (typeof value !== "function") {
+    continue;
+  }
+  const descriptor = Object.getOwnPropertyDescriptor(value, "prototype");
+  if (descriptor && !descriptor.writable) {
+    if (!(key in kolmafiaClasses)) {
+      console.warn("Missing MafiaClass from kolmafia mock:", key);
+    }
+    continue;
+  }
 
-export const print = jest.fn((msg) => console.log(`[kolmafia.print] ${msg}`));
+  kolmafiaMocks[key] = jest.fn();
+}
 
-export const toItem = jest.fn((name) => Item.get(name));
-export const visitUrl = jest.fn();
+const kolmafia = kolmafiaMocks as Mocked<Kolmafia>;
+
+kolmafia.logprint.mockImplementation((msg) =>
+  console.log(`[kolmafia.logprint] ${msg}`)
+);
+kolmafia.print.mockImplementation((msg?) =>
+  console.log(`[kolmafia.print] ${msg}`)
+);
+kolmafia.printHtml.mockImplementation((msg) =>
+  console.log(`[kolmafia.printHtml] ${msg}`)
+);
+
+kolmafia.getMonsters.mockImplementation(() => []);
+kolmafia.nowToString.mockImplementation(() => new Date().toISOString());
+kolmafia.toInt.mockImplementation((value) => parseInt(value as any, 10));
+kolmafia.toPlural.mockImplementation((item) => item.plural);
+kolmafia.toString.mockImplementation((value) => value.toString());
+
+const mockProperties = new Map<string, string>();
+export const clearMockProperties = () => mockProperties.clear();
+kolmafia.getProperty.mockImplementation((key) => mockProperties.get(key) ?? "");
+kolmafia.setProperty.mockImplementation((key, value) =>
+  mockProperties.set(key, value)
+);
+
+module.exports = kolmafia;

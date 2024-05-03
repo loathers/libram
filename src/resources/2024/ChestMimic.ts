04e2eeee60ccb83c4caf3dbb86bba12922e26550
Ignose <ihpares@gmail.com>
2024-05-03 11:44:09 -0400

    I guess we don't need the name at all then, huh

--- src/resources/2024/ChestMimic.ts
+++ src/resources/2024/ChestMimic.ts
@@ -142,7 +142,7 @@ export function differentiate(
 export function differentiableQuantity(monster: Monster): number {
   if (!have_($item`mimic egg`)) return 0;
   const regex = new RegExp(
-    `${monster.name}<!--monsterid:${monster.id}-->(?:\\s\\((\\d+)\\))?`
+    `<!--monsterid:${monster.id}-->(?:\\s\\((\\d+)\\))?`
   );
   const page = examine($item`mimic egg`);
   const match = page.match(regex);

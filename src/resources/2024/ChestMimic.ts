25276d2e22eb724e5ec2870ce37979f6d6627b8c
Ignose <ihpares@gmail.com>
2024-05-03 11:36:37 -0400

    This regex works and stuff.

--- src/resources/2024/ChestMimic.ts
+++ src/resources/2024/ChestMimic.ts
@@ -141,9 +141,7 @@ export function differentiate(
  */
 export function differentiableQuantity(monster: Monster): number {
   if (!have_($item`mimic egg`)) return 0;
-  const regex = new RegExp(
-    `<!-- monsterid: ${monster.id}-->(?:\\s\\((\\d+)\\))?`
-  );
+  const regex = new RegExp(`${monster.name}(?:\\s\\((\\d+)\\))?`);
   const page = examine($item`mimic egg`);
   const match = page.match(regex);
   if (!match) return 0;

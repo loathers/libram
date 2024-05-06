f577d7e9b75472833fa3bce00130f7e34e9a6a09
horrible little slime <69secret69email69@gmail.com>
2024-05-05 23:22:48 -0400

    more functions and exports

--- src/resources/2024/MayamCalendar.ts
+++ src/resources/2024/MayamCalendar.ts
@@ -1,14 +1,14 @@
-import { cliExecute } from "kolmafia";
+import { cliExecute, Effect, Item } from "kolmafia";
 import { have as have_ } from "../../lib";
 import { get } from "../../property";
-import { $item } from "../../template-string";
+import { $effect, $item } from "../../template-string";
 
-const RINGS = [
+export const RINGS = Object.freeze([
   ["yam1", "sword", "eye", "chair", "fur", "vessel"],
   ["lightning", "bottle", "meat", "wood", "yam2"],
   ["yam3", "wall", "cheese", "eyepatch"],
   ["explosion", "clock", "yam4"],
-] as const;
+] as const);
 
 export type MayamSymbol = typeof RINGS[number][number];
 export type Ring<N extends number> = typeof RINGS[N][number];
@@ -71,4 +71,31 @@ export function submit(
     return false;
   }
   return cliExecute(`mayam ring ${combination.join(" ")}`);
+}
+
+export const RESONANCES = Object.freeze({
+  "eye yam2 eyepatch yam4": $item`Mayam spinach`,
+  "vessel yam2 cheese explosion": $item`stuffed yam stinkbomb`,
+  "yam1 meat cheese yam4": $item`yam and swiss`,
+  "sword yam2 eyepatch explosion": $item`yam cannon`,
+  "fur lightning eyepatch yam4": $item`tiny yam cannon`,
+  "yam1 lightning yam3 clock": $item`yam battery`,
+  "fur yam2 wall yam4": $item`furry yam buckler`,
+  "yam1 yam2 yam3 explosion": $item`thanksgiving bomb`,
+  "yam1 meat eyepatch yam4": $item`yamtility belt`,
+  "chair yam2 yam3 clock": $effect`caught yam-handed`,
+  "yam1 yam2 cheese clock": $effect`memories of a cheesier age`,
+} as const);
+
+/**
+ * Find the combination needed to get a particular resonance
+ * @param target The Item or Effect granted by the resonance
+ * @returns The combination needed, or null if none such exists
+ */
+export function resonanceFor(target: Item | Effect): Combination | null {
+  return (
+    ([...Object.entries(RESONANCES)]
+      .find(([, value]) => value === target)?.[0]
+      .split(" ") as Combination) ?? null
+  );
 }

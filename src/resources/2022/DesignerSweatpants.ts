ce8db74ceef6af1ac3062d72c2f7d1656c256dd8
Joe Kirchoff <Kirchoff.Joseph.P@gmail.com>
2025-03-29 11:52:56 -0700

    Add designer sweatpants support

---
+++ src/resources/2022/DesignerSweatpants.ts
@@ -0,0 +1,88 @@
+import {
+  availableAmount,
+  Skill,
+  useSkill as useSkill_,
+  visitUrl,
+} from "kolmafia";
+import { get } from "../../property.js";
+import { $item, $skill } from "../../template-string.js";
+
+export const item = $item`designer sweatpants`;
+
+export const skills = {
+  SweatFlick: $skill`Sweat Flick`,
+  SweatSpray: $skill`Sweat Spray`,
+  SweatFlood: $skill`Sweat Flood`,
+  SipSomeSweat: $skill`Sip Some Sweat`,
+  SweatSip: $skill`Sweat Sip`,
+  DrenchYourselfInSweat: $skill`Drench Yourself in Sweat`,
+  SweatOutSomeBooze: $skill`Sweat Out Some Booze`,
+  MakeSweatAde: $skill`Make Sweat-Ade`,
+} as const;
+
+const sweatSkillCost = new Map<Skill, number>([
+  [$skill`Sweat Flick`, 1],
+  [$skill`Sweat Spray`, 3],
+  [$skill`Sweat Flood`, 5],
+  [$skill`Sip Some Sweat`, 5],
+  [$skill`Sweat Sip`, 5],
+  [$skill`Drench Yourself in Sweat`, 15],
+  [$skill`Sweat Out Some Booze`, 25],
+  [$skill`Make Sweat-Ade`, 50],
+]);
+
+/**
+ * Do you own designer sweatpants
+ * @returns Whether you have the designer sweatpants
+ */
+export function have(): boolean {
+  return availableAmount(item) > 0;
+}
+
+/**
+ * Get the current sweat amount
+ * @returns The amount of sweat
+ */
+export function sweat(): number {
+  return get("sweat");
+}
+
+/**
+ * Refresh the current sweat amount by visiting the item description
+ */
+export function refresh(): void {
+  visitUrl(`desc_item.php?whichitem=${item.descid}`);
+}
+
+/**
+ * Get the sweat cost of a skill
+ * @param skill The skill to check
+ * @returns The sweat cost, or Infinty for an invalid skill
+ */
+export function sweatCost(skill: Skill): number {
+  return sweatSkillCost.get(skill) ?? Infinity;
+}
+
+/**
+ * Check if a sweat skill can be cast
+ * @param skill The skill to check
+ * @returns If this skill can be cast
+ */
+export function canUseSkill(skill: Skill): boolean {
+  return have() && skill.dailylimit !== 0 && sweat() <= sweatCost(skill);
+}
+
+/**
+ * Cast a sweat skill, on failure refresh sweat amount
+ * @param skill The skill to cast
+ * @returns If this skill was cast successfully
+ */
+export function useSkill(skill: Skill): boolean {
+  if (!canUseSkill(skill)) return false;
+  const sweatAmt = sweat();
+  const result = useSkill_(1, skill) && sweatAmt !== sweat();
+  if (!result) {
+    refresh();
+  }
+  return result;
+}

f41ed2471ecd2c51ea8de672206588f8e44be869
horrible little slime <69secret69email69@gmail.com>
2025-06-08 23:03:12 -0400

    rename for clarity

--- src/resources/2025/Beret.ts
+++ src/resources/2025/Beret.ts
@@ -7,6 +7,7 @@ import {
   Item,
   toSlot,
   npcPrice,
+  canEquip,
 } from "kolmafia";
 import {
   $effect,
@@ -41,27 +42,34 @@ export function have(): boolean {
   return have_(beret);
 }
 
-function beretPowerSum(buyItem: boolean): number[] {
+function availablePowersums(buyItem: boolean): number[] {
   const taoMultiplier = have_($skill`Tao of the Terrapin`) ? 2 : 1;
 
-  const allItems = Item.all().filter(
-    (i) => have_(i) || (buyItem && npcPrice(i) > 0),
+  const availableItems = Item.all().filter(
+    (i) => canEquip(i) && (have_(i) || (buyItem && npcPrice(i) > 0)),
   );
-  const allHats = have_($familiar`Mad Hatrack`)
-    ? allItems.filter((i) => toSlot(i) === $slot`hat`)
+  const useableHats = have_($familiar`Mad Hatrack`)
+    ? availableItems.filter((i) => toSlot(i) === $slot`hat`)
     : [beret];
-  const allPants = allItems.filter((i) => toSlot(i) === $slot`pants`);
-  const allShirts = allItems.filter((i) => toSlot(i) === $slot`shirt`);
+  const useablePants = availableItems.filter((i) => toSlot(i) === $slot`pants`);
+  const useableShirts = availableItems.filter(
+    (i) => toSlot(i) === $slot`shirt`,
+  );
 
-  const hats = [...new Set(allHats.map((i) => taoMultiplier * getPower(i)))];
-
-  const pants = [...new Set(allPants.map((i) => taoMultiplier * getPower(i)))];
-  const shirts = [...new Set(allShirts.map((i) => getPower(i)))];
+  const hatPowers = [
+    ...new Set(useableHats.map((i) => taoMultiplier * getPower(i))),
+  ];
+  const pantPowers = [
+    ...new Set(useablePants.map((i) => taoMultiplier * getPower(i))),
+  ];
+  const shirtPowers = [...new Set(useableShirts.map((i) => getPower(i)))];
 
   return [
     ...new Set(
-      hats.flatMap((hat) =>
-        pants.flatMap((pant) => shirts.flatMap((shirt) => hat + pant + shirt)),
+      hatPowers.flatMap((hat) =>
+        pantPowers.flatMap((pant) =>
+          shirtPowers.flatMap((shirt) => hat + pant + shirt),
+        ),
       ),
     ),
   ];
@@ -77,23 +85,23 @@ function scoreBusk(
 }
 
 /**
- * Calculate the optimal power at which to busk, given a weighted set of modifiers.
+ * Calculate the optimal power-sum at which to busk, given a weighted set of modifiers.
  * @param effectValuer Either a function that maps effects to values, or an object keyed by numeric modifiers with weights as values
  * @param buskUses accepts an input of number (0-5) to determine which busk to check; default behavior is to check the next available busk
  * @param uselessEffects accepts an input of Effect[] and values those effects at 0 for scoring
  * @param buyItem boolean, determines if we should check shop available equipment; defaults to true
- * @returns The power at which you'll find the optimal busk for this situation.
+ * @returns The power-sum at which you'll find the optimal busk for this situation.
  */
-export function findOptimalBusks(
+export function findOptimalCasts(
   effectValuer: EffectValuer,
   buskUses = get("_beretBuskingUses"),
   uselessEffects: Effect[] = [],
   buyItem: boolean = true,
 ): number {
   const uselessEffectSet = new Set(uselessEffects);
-  const powersum = beretPowerSum(buyItem);
-  if (!powersum.length) return 0;
-  return maxBy(powersum, (power) =>
+  const powersums = availablePowersums(buyItem);
+  if (!powersums.length) return 0;
+  return maxBy(powersums, (power) =>
     scoreBusk(
       Object.keys(beretBuskingEffects(power, buskUses))
         .map((e) => toEffect(e))

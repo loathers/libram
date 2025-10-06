7a0f5eff4df773ace9330973303d0345a82491f8
Ignose <ihpares@gmail.com>
2025-10-06 15:23:29 -0400

    Tuples => Maps

--- src/resources/2025/BloodCubicZirconia.ts
+++ src/resources/2025/BloodCubicZirconia.ts
@@ -3,13 +3,13 @@ import { $item, $skill, $skills, $stat } from "../../index.js";
 import { have as have_ } from "../../lib.js";
 
 /**
- * @returns Whether or not you have the crepe paper parachute cape
+ * @returns Whether or not you have the blood cubic zirconia
  */
 export function have(): boolean {
   return have_($item`blood cubic zirconia`);
 }
 
-const BCZSKILLCOST = [
+const BCZSKILLCOST = new Map<number, number>([
   [0, 11],
   [1, 23],
   [2, 37],
@@ -32,10 +32,10 @@ const BCZSKILLCOST = [
   [19, 110000000],
   [20, 230000000],
   [21, 370000000],
-];
+]);
 
 const BCZSKILLS = $skills`BCZ: Blood Geyser, BCZ: Refracted Gaze, BCZ: Sweat Bullets, BCZ: Blood Bath, BCZ: Craft a Pheromone Cocktail, BCZ: Create Blood Thinner, BCZ: Dial it up to 11, BCZ: Prepare Spinal Tapas, BCZ: Sweat Equity`;
-export const BCZCOSTS: [Skill, Stat][] = [
+export const BCZCOSTS = new Map<Skill, Stat>([
   [$skill`BCZ: Blood Geyser`, $stat`Muscle`],
   [$skill`BCZ: Refracted Gaze`, $stat`Mysticality`],
   [$skill`BCZ: Sweat Bullets`, $stat`Moxie`],
@@ -45,7 +45,7 @@ export const BCZCOSTS: [Skill, Stat][] = [
   [$skill`BCZ: Dial it up to 11`, $stat`Mysticality`],
   [$skill`BCZ: Prepare Spinal Tapas`, $stat`Mysticality`],
   [$skill`BCZ: Sweat Equity`, $stat`Moxie`],
-];
+]);
 
 /**
  * @param skill The BCZ skill to check.
@@ -56,17 +56,18 @@ export function availableCasts(skill: Skill, statFloor: number): number {
   if (!have()) return 0;
   if (!BCZSKILLS.includes(skill)) return 0;
 
-  const stat = BCZCOSTS.find(([s]) => s === skill)?.[1];
+  const stat = BCZCOSTS.get(skill);
   if (!stat) return 0;
 
   const currentStat = myBasestat(stat);
-  const timesCast = skill.timescast;
+  const timesCast = skill.timescast ?? 0;
 
   let casts = 0;
   let remainingStat = currentStat;
 
-  for (let i = timesCast; i < BCZSKILLCOST.length; i++) {
-    const nextCost = BCZSKILLCOST[i][1];
+  for (let i = timesCast; i < BCZSKILLCOST.size; i++) {
+    const nextCost = BCZSKILLCOST.get(i);
+    if (nextCost === undefined) break;
     if (remainingStat - nextCost < statFloor) break;
     remainingStat -= nextCost;
     casts++;
